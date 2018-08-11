fe8dbb33abfa520a01e3705bdc8ed0d35fc3e986
iakshayy1 <vontariakshayreddy184@gmail.com>
2018-08-11 15:23:04 -0500

    added changes

--- models/product.js
+++ models/product.js
@@ -1,12 +1,12 @@
 // Model for product
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-    category: { type: String, required: true },
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true }
+  category: { type: String, required: true },
+  title: { type: String, required: true },
+  description: { type: String },
+  price: { type: Number, required: true }
 });
 
-module.exports = mongoose.model('Product', schema);
+module.exports = mongoose.model("Product", schema);
